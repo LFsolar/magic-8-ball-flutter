0784dc0ac456813c7bff17d3791d06430886b42f
Lisa Sun <sunlisa@gmail.com>
2022-01-22 22:51:10 -0600

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: const Text(
+              'Ask Me Anything',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNum = 1;
+
+  void shakeBall() {
+    setState(() {
+      ballNum = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextButton(
+          onPressed: () => shakeBall(),
+          child: Image.asset('images/ball$ballNum.png'),
+        ),
+      ),
+    );
+  }
+}
